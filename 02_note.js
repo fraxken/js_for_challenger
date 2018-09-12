4b5a91985ec3c31ad24de8f7b21e7248a2545660
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-12 18:17:29 +0200

    Fix typo in introductions

--- 02_note.js
+++ 02_note.js
@@ -2,8 +2,8 @@
  * Note des élèves (sur /20) d'une classe de CM1.
  * 
  * Exercice: 
- * 1. Calculer la moyenne de cet classe.
- * 2. Arondir le résultat vers le bas (avec la function Math.floor).
- * 3. Afficher le résultat dans la console (le terminal).
+ * 1. Calculer la moyenne du tableau notesDesEleves.
+ * 2. Arondir la moyenne vers le bas (avec la function Math.floor).
+ * 3. Afficher le résultat (la moyenne) sur 20 dans la console (le terminal).
  */
 const notesDesEleves = [10, 20, 5, 10, 3, 1, 0, 11, 8, 18, 15];
--- 03_notev2.js
+++ 03_notev2.js
@@ -2,10 +2,10 @@
  * Note des élèves (sur /20) d'une classe de CM1.
  * 
  * Exercices:
- * Plusieurs erreurs ont été comise par l'établissement. Les moyennes auraient du être sur 10 (et pas sur 20).
+ * Plusieurs erreurs ont été comise par l'établissement. Les notes auraient du être sur 10 (et non sur 20).
  * 1. Pour compenser cet erreur chaque note recevra un bonus de 0.5 points (sur /10).
- * 2. Quelle est la moyenne (sur /10) ?
+ * 2. Calculer la moyenne des notes sur /10
  * 3. La moyenne doit être arondit vers le bas !
- * 4. Afficher le résultat dans la console!
+ * 4. Afficher le résultat (la moyenne) sur /10 dans la console (le terminal).
  */
 const notesDesEleves = [10, 20, 4, 10, 3, 1, 0, 11, 8, 18, 15];
