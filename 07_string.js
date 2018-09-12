d573853a9e4ca1de165c29711d9e13a17783ffcb
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-09-12 18:30:52 +0200

    Replace voiture exo

---
+++ 07_string.js
@@ -0,0 +1,20 @@
+let myStr = `         Hello wOrld! \n fOObar!   \n pomme orange    `;
+
+/**
+ * Exercices:
+ * 
+ * @doc: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String
+ * 
+ * !! MERCI DE RESPECTER L'ORDRE DES ETAPES !!
+ * 
+ * 0. Remplacer la sous chaine "pomme orange" par "pomme verte"
+ * 1. Séparer la chaine par retour chariot "\n" avec la méthode .split
+ * 2. Supprimer les espaces non nécessaires dans chaque sous-chaîne. 
+ * 3. Recombiner les sous-chaines en une seul chaîne en les séparant d'une virgule !
+ * 4. Assurez-vous que les caractères de la chaine soi en minuscule.
+ * 5. Afficher la taille de la chaine dans la console (le terminal).
+ * 6. Afficher la valeur de la chaine dans la console (le terminal).
+ */
+
+ // Etape 0 -> Compléter/continuer le code
+ myStr = myStr.replace("?", "?");
--- 09_date.js
+++ 09_date.js
@@ -3,11 +3,14 @@
  * - Trier les dates de la plus vielle à la plus récente.
  */
 
-// Liste de Dates
-const DatesList = [
+// Tableau de dates
+const DatesArr = [
     new Date("2017-29-06 00:30:00"),
     new Date("2017-29-06 00:00:00"),
     new Date("2017-05-06 00:00:00"),
     new Date("2015-29-06 00:00:00"),
     new Date("2019-29-06 08:00:00")
 ];
+
+// Afficher les dates:
+console.log(DatesArr);
